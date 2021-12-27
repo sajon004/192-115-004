8383c630d82ae3c1d9594b1036fbdba177264af9
sajon004 <sajonahmed0462@gmail.com>
2021-12-27 15:44:44 +0600

    load assets

--- my_app/server.js
+++ my_app/server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
 
 const app = express();
 
 dotenv.config({path:'config.env'})
-const port  = process.env.port||8080
+const port  = process.env.port||7593
 
 //log_requests
 app.use(morgan('tiny'));
+
+//request to body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+//set view
+app.set("view engine","ejs")
+//app.set("views",path.resolve(__dirname,"views/ejs"))
+
+//load assests
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
 
 app.get('/',(req,res)=>{
     res.send("Library Management System");
